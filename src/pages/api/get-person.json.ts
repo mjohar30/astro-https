c630aa6dbbfdbd2e82e16096924de2815de40ae0
Mario Johar González Pedraza <mjohar30@gmail.com>
2026-06-02 17:01:07 -0600

    Add Astro Node adapter and update API routes for blog posts

---
+++ src/pages/api/get-person.json.ts
@@ -0,0 +1,16 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const person = {
+    name: 'Mario Gonzalez',
+    age: 30
+  }
+  return new Response(JSON.stringify(person), 
+    { 
+      status: 200, 
+      headers: { 
+        'Content-Type': 'application/json' 
+      } 
+    }
+  )
+}
---
+++ src/pages/api/posts/index.ts
@@ -0,0 +1,46 @@
+import type { APIRoute } from "astro";
+import { getCollection, getEntry } from "astro:content";
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ params, request }) => {
+  
+  
+  console.log(request)
+  // console.log(url)
+  
+  const url = new URL(request.url)
+  const id = url.searchParams.get('id')
+  
+  if(id){
+    const exactPost = await getEntry('blog', id)
+    
+    if(exactPost){
+      return new Response(JSON.stringify(exactPost), {
+        status: 200,
+        headers: { 
+          'Content-Type': 'application/json' 
+        } 
+      })
+    } else {
+      return new Response(JSON.stringify({ msg: `Post ${id} not found`}), {
+        status: 404,
+        headers: { 
+          'Content-Type': 'application/json' 
+        } 
+      })
+    }
+  }
+
+  const posts = await getCollection('blog')
+
+  return new Response(JSON.stringify(posts), 
+    { 
+      status: 200, 
+      headers: { 
+        'Content-Type': 'application/json' 
+      } 
+    }
+  )
+
+}
